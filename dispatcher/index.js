34a40e0da80f79f84203a1eac5ca6446c19aa493
William Buchwalter <wbuchwalter@gmail.com>
2016-07-30 11:05:46 -0700

    change binding dispatcher

--- dispatcher/index.js
+++ dispatcher/index.js
@@ -1,13 +1,13 @@
 var http = require('https');
 
-module.exports = function (context, issue) {
+module.exports = function (context, issue, tagsTable) {
      var post_options = {
       host: 'hooks.slack.com',
       port: '443',
       path: '/services/T1WKD9CK1/B1WKDL40P/UTQ6QCBRqETCJfTiObHfWCor',
       method: 'POST'
     };
-
+    context.log(tagsTable);
     var req = http.request(post_options, res => {});
     req.on('error', function(e) {
         context.log('problem with request: ' + e.message);
