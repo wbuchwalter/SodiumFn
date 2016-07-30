d6e91cc5d97486d4aaa50f156d6f222dd6b68f82
William Buchwalter <wbuchwalter@gmail.com>
2016-07-29 17:01:42 -0700

    add dispatcher

--- dispatcher/index.js
+++ dispatcher/index.js
@@ -1,4 +1,20 @@
-module.exports = function (context, myQueueItem) {
-    context.log('Node.js queue trigger function processed work item', myQueueItem);
+var http = require('https');
+
+module.exports = function (context, issue) {
+     var post_options = {
+      host: 'hooks.slack.com',
+      port: '443',
+      path: '/services/T1WKD9CK1/B1WKDL40P/UTQ6QCBRqETCJfTiObHfWCor',
+      method: 'POST'
+    };
+
+    var req = http.request(post_options, res => {});
+    req.on('error', function(e) {
+        context.log('problem with request: ' + e.message);
+    });
+
+    req.write(`{"channel": "@will", "text": " You've got some work to do! : <${issue.link}|${issue.title}>"}`);
+    req.end();
+
     context.done();
 };
